7f794df90c8e1459ea4ff38398d62f99300e46c3
Linkfinitive <116618526+Linkfinitive@users.noreply.github.com>
2025-12-15 17:24:14 +1100

    Interop working perfectly - website load now gives javascript an array of the names of the free locations. now just have to make the frontend hehe.

---
+++ Backend/DataManager.cs
@@ -0,0 +1,57 @@
+using System.Runtime.InteropServices.JavaScript;
+
+namespace StudySpot.Backend;
+
+public static partial class DataManager
+{
+    static readonly List<Entry> entries = new();
+
+    [JSExport]
+    static void Load(string fileContents)
+    {
+        //Files begin with 5 lines of heading befor the actual data.
+        const int linesOfHeadingInFile = 5;
+
+        //Using statement will automatically close the reader even if there is an exception (creates a try/finally block behind the scenes).
+        using StringReader reader = new(fileContents);
+
+        //Skip any header lines in the file.
+        for (int i = 0; i < linesOfHeadingInFile; i++) reader.ReadLine();
+
+        //Assigns the read line to the line variable but ends the loop if the line is null.
+        while (reader.ReadLine() is { } line)
+        {
+            entries.Add(new Entry(line));
+        }
+
+        Console.WriteLine("Read in Successfully");
+    }
+
+    [JSExport]
+    static string[] GetFreeLocations(string dateTimeString)
+    {
+        DateTime dateTime = DateTime.Parse(dateTimeString);
+
+        Location[] unfilteredLocations = Location.SortEntriesByLocation(entries.ToArray());
+        Location[] locations = Location.RemoveNonClassroomLocations(unfilteredLocations);
+
+        int teachingWeek = TimeManager.GetTeachingWeek(dateTime);
+        DayOfWeek dayOfWeek = dateTime.DayOfWeek;
+        TimeOnly time = TimeOnly.FromDateTime(dateTime);
+
+        List<Location> locationsCurrentlyFree = new();
+        foreach (Location l in locations)
+        {
+            if (l.IsFreeAt(time, dayOfWeek, teachingWeek))
+            {
+                locationsCurrentlyFree.Add(l);
+                Console.WriteLine($"{l.Name} is free at this time.");
+            }
+        }
+
+        Console.WriteLine($"{locationsCurrentlyFree.Count} free locations found of {locations.Length} searched.");
+        Console.WriteLine($"Time searched: {dateTime}");
+
+        return Location.LocationListToStringOfLocationNames(locationsCurrentlyFree);
+    }
+}
---
+++ wwwroot/main.js
@@ -0,0 +1,35 @@
+//The number of CSV files holding the entire data. Needs to be updated the number of CSV files changes.
+const numberOfCsvFiles = 10;
+
+//Import the dotnet bootstrapper from where it is output by the compiler.
+import {dotnet} from "./_framework/dotnet.js";
+
+//Initialise dotnet runtime and get the configuration.
+const {getAssemblyExports, getConfig} = await dotnet.create();
+const config = getConfig();
+
+//Import the [JSExport] functions from C#.
+const exports = await getAssemblyExports(config.mainAssemblyName);
+
+async function loadCSV() {
+
+    //Load in each file one at a time.
+    for (let i = 1; i <= numberOfCsvFiles; i++) {
+
+        //Fetch the data from the CSV files one at a time
+        const response = await fetch(`./CsvFiles/${i}.csv`);
+        const csvText = await response.text();
+
+        //Send to dotnet
+        exports.StudySpot.Backend.DataManager.Load(csvText);
+    }
+}
+
+function getFreeLocations(dateTime) {
+    return exports.StudySpot.Backend.DataManager.GetFreeLocations(dateTime.toISOString());
+}
+
+//Program
+await loadCSV();
+let freeLocations = getFreeLocations(new Date());
+console.log(freeLocations);
