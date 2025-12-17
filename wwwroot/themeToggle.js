785879e64d8d09ea83e3b713d44894574c8b03cb
Toitle419 <105914122@student.swin.edu.au>
2025-12-17 11:55:08 +1100

    changed toggleTheme behaviour to ensure theme is set before page first renders, removing the flash of light theme on page load

--- wwwroot/themeToggle.js
+++ wwwroot/themeToggle.js
@@ -1,27 +1,22 @@
 const htmlElement = document.documentElement;
-const toggleButton = document.getElementById('themeToggle');
-if (!toggleButton) { throw new Error('No element with ID "themeToggle" found'); }
-else {
-    // Load saved theme preference
-    const savedTheme = localStorage.getItem('theme');
-    if (savedTheme) {
-        setTheme(savedTheme);
-    }
+// Load saved theme preference
+const savedTheme = localStorage.getItem('theme');
+if (savedTheme) { setTheme(savedTheme); }
+else { setTheme('light'); } // Default theme
 
-    // Toggle theme on button click
-    toggleButton.addEventListener('click', () => {
-        const currentTheme = htmlElement.getAttribute('data-theme');
-        const newTheme = currentTheme === 'light' ? 'dark' : 'light';
-        setTheme(newTheme);
-    });
+// function to toggle theme
+function toggleTheme() {
+    const currentTheme = htmlElement.getAttribute('data-theme');
+    const newTheme = currentTheme === 'light' ? 'dark' : 'light';
+    setTheme(newTheme);
 }
 
 // Function to set the theme
 function setTheme(theme) {
+    htmlElement.setAttribute('data-theme', theme);
+    localStorage.setItem('theme', theme); // Save preference
+    // set button image based on theme
+    toggleButton = document.getElementById('themeToggle');
     if (!toggleButton) { throw new Error('No element with ID "themeToggle" found'); }
-    else {
-        htmlElement.setAttribute('data-theme', theme);
-        localStorage.setItem('theme', theme); // Save preference
-        toggleButton.setAttribute('src', theme === 'light' ? 'images/LightThemeButton.png' : 'images/DarkThemeButton.png');
-    }
+    else { toggleButton.setAttribute('src', theme === 'light' ? 'images/LightThemeButton.png' : 'images/DarkThemeButton.png'); }
 }
